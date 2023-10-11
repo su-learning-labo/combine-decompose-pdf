57ebf39c662f7e42c4bebd5566512ec5eedd80bd
su-leaning-lab <groovin_high@i-iide.com>
2023-10-11 21:26:34 +0900

    update functions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from pypdf import PdfReader, PdfWriter, PdfMerger
 
 
@@ -9,109 +8,43 @@ def get_page_count(pdf_file):
     return len(pdf_reader.pages)
 
 
-# ファイルサイズの取得（MB）
-def format_size(size_bytes):
-    size_mb = size_bytes / (1024 * 1024)
-    return f'{round(size_mb, 1)}MB'
-
-
 # PDfファイルの結合
-def merge_pdf(base_pdf):
-    file_name_str = st.text_input('出力するPDFファイル名を入力してください（拡張子.pdfは不要）', placeholder='Please enter file name.')
-    output_file_name = f'{file_name_str}.pdf'
+def merge_pdf(base_pdf, add_pdfs, output_file_name):
+    merger = PdfMerger()
+    merger.append(base_pdf)
 
-    add_pdfs = st.file_uploader(
-        "結合するPDFファイルをアップロードしてください",
-        type='pdf',
-        accept_multiple_files=True
-    )
+    for add_pdf in add_pdfs:
+        merger.append(add_pdf)
 
-    if add_pdfs:
-        merger = PdfMerger()
-        merger.append(base_pdf)
-
-        for add_pdf in add_pdfs:
-            merger.append(add_pdf)
-
-        merger.write(output_file_name)
-
-        st.success("PDFの結合が完了し、新しいPDFが保存されました。ダウンロードしてください")
-        download_file(output_file_name)
+    merger.write(output_file_name)
 
 
 # PDfファイルの削除
-def delete_pages(pdf_file, count_pages):
+def delete_pages(pdf_file, page_numbers, output_file_name):
+    pages_to_delete = sorted([int(page.strip()) for page in page_numbers.split(",")])
 
-    file_name_str = st.text_input('出力するPDFファイル名を入力してください（拡張子.pdfは不要）', placeholder='Please enter file name.')
-    output_file_name = f'{file_name_str}.pdf'
+    pdf_reader = PdfReader(pdf_file)
+    pdf_writer = PdfWriter()
 
-    try:
-        page_numbers = st.text_input(
-            label='削除するページ番号をカンマ区切りで入力してください（例: 2, 5, 7）'
-        )
+    for page_num, page in enumerate(pdf_reader.pages, start=1):
+        if page_num not in pages_to_delete:
+            pdf_writer.add_page(page)
 
-        st.write(f'アップロードファイルのページ数: {count_pages}')
-
-        if page_numbers:
-            # pages = [n for n in range(1, count_pages + 1)]
-            pages_to_delete = sorted([int(page.strip()) for page in page_numbers.split(",")])
-
-            pdf_reader = PdfReader(pdf_file)
-            pdf_writer = PdfWriter()
-
-            st.write(f'削除ページ: {pages_to_delete}')
-            for page_num in range(1, count_pages + 1):
-                if page_num not in pages_to_delete:
-                    page = pdf_reader.pages[page_num - 1]
-                    pdf_writer.add_page(page)
-
-            pdf_writer.write(output_file_name)
-
-            st.success("PDFの結合が完了し、新しいPDFが保存されました。ダウンロードしてください")
-            download_file(output_file_name)
-
-    except Exception as e:
-        st.error(f"エラーが発生しました: {str(e)}")
+    pdf_writer.write(output_file_name)
 
 
 # ファイルの並び替え
-def reorder_pages(pdf_file, count_pages):
+def reorder_pages(pdf_file, page_order, output_file_name):
+    page_order = [int(page.strip()) for page in page_order.split(",")]
 
-    file_name_str = st.text_input('出力するPDFファイル名を入力してください（拡張子.pdfは不要）', placeholder='Please enter file name.')
-    output_file_name = f'{file_name_str}.pdf'
+    pdf_reader = PdfReader(pdf_file)
+    pdf_writer = PdfWriter()
 
-    try:
-        page_order = st.text_input(
-            label='ページの並びをカンマ区切りで指定してください（例: 2, 5, 7）',
-        )
+    for page_num in page_order:
+        page = pdf_reader.pages[page_num - 1]
+        pdf_writer.add_page(page)
 
-        st.write(f'元のページ順: {[n for n in range(1, count_pages + 1)]}')
-        if page_order:
-            page_order = [int(page.strip()) for page in page_order.split(",")]
-
-            if set(page_order) == set(range(1, count_pages + 1)):
-
-                st.write(f' → 指定の並び順: {page_order}')
-
-                pdf_reader = PdfReader(pdf_file)
-                pdf_writer = PdfWriter()
-
-                for page_num in page_order:
-                    page = pdf_reader.pages[page_num - 1]
-                    pdf_writer.add_page(page)
-
-                # 新しいPDFファイルを保存
-                pdf_writer.write(output_file_name)
-
-                st.success("ページの並び替えが完了し、新しいPDFが保存されました。ダウンロードしてください")
-
-                download_file(output_file_name)
-
-            else:
-                st.warning('ページの指定が誤っています。確認してください。')
-
-    except Exception as e:
-        st.error(f"エラーが発生しました: {str(e)}")
+    pdf_writer.write(output_file_name)
 
 
 def download_file(file_path):
@@ -133,7 +66,6 @@ def main():
     base_pdf = st.file_uploader("PDFファイルをアップロードしてください", type="pdf")
 
     if base_pdf:
-
         # アップロードファイルのページ数取得
         count_pages = get_page_count(base_pdf)
 
@@ -145,20 +77,55 @@ def main():
             index=None
         )
 
-        # 結合処理
         if selector == '結合':
             st.write('---')
-            merge_pdf(base_pdf)
+            file_name_str = st.text_input('出力するPDFファイル名を入力してください（拡張子.pdfは不要）', placeholder='Please enter file name.')
+            output_file_name = f'{file_name_str}.pdf'
+            add_pdfs = st.file_uploader(
+                "結合するPDFファイルをアップロードしてください",
+                type='pdf',
+                accept_multiple_files=True
+            )
 
-        # ページ削除
-        if selector == 'ページ削除':
+            if st.button("結合"):
+                if add_pdfs:
+                    merge_pdf(base_pdf, add_pdfs, output_file_name)
+                    st.success("PDFの結合が完了し、新しいPDFが保存されました。ダウンロードしてください")
+                    download_file(output_file_name)
+
+        elif selector == 'ページ削除':
             st.write('---')
-            delete_pages(base_pdf, count_pages)
+            page_number_list = [n for n in range(1, count_pages + 1)]
+            file_name_str = st.text_input('出力するPDFファイル名を入力してください（拡張子.pdfは不要）',
+                                          placeholder='Please enter file name.')
+            output_file_name = f'{file_name_str}.pdf'
+            page_numbers = st.text_input(
+                        label='削除するページ番号をカンマ区切りで入力してください（例: 2, 5, 7）',
+                        placeholder=f'削除対象ページ番号: {page_number_list}'
+                    )
+
+            if st.button("ページ削除"):
+                if page_numbers:
+                    delete_pages(base_pdf, page_numbers, output_file_name)
+                    st.success("PDFのページ削除が完了し、新しいPDFが保存されました。ダウンロードしてください")
+                    download_file(output_file_name)
 
         # 並び替え
-        if selector == '並び替え':
+        elif selector == '並び替え':
             st.write('---')
-            reorder_pages(base_pdf, count_pages)
+            file_name_str = st.text_input('出力するPDFファイル名を入力してください（拡張子.pdfは不要）', placeholder='Please enter file name.')
+            output_file_name = f'{file_name_str}.pdf'
+            page_number_list = [n for n in range(1, count_pages + 1)]
+            page_order = st.text_input(
+                label='ページの並び順をカンマ区切りで指定してください（例: 2, 5, 7）',
+                placeholder=f'ページ番号: {page_number_list}'
+            )
+
+            if st.button("並び替え"):
+                if page_order:
+                    reorder_pages(base_pdf, page_order, output_file_name)
+                    st.success("PDFのページ並び替えが完了し、新しいPDFが保存されました。ダウンロードしてください")
+                    download_file(output_file_name)
 
 
 if __name__ == "__main__":
